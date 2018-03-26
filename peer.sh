bf6904ed3f9040a467617dcff428ab240852ce25
Frikitrok <zgrabovskiy@gmail.com>
2018-03-26 15:49:28 +0300

    asd

---
+++ peer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update && apt upgrade -y && apt install -y htop
+wget -O splunk-7.0.2-03bbabbd5c0f-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.2&product=splunk&filename=splunk-7.0.2-03bbabbd5c0f-linux-2.6-amd64.deb&wget=true'
+sudo dpkg -i splunk*.deb
+cd /opt/splunk
+./bin/splunk start --accept-license
+./bin/splunk enable boot-start
+./bin/splunk edit cluster-config -mode slave -master_uri https://splunk-node:8089 -replication_port 9887 -secret frikitrok -auth admin:changeme
+./bin/splunk restart 
